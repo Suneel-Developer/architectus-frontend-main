5fb0ff452f424293087a908e9396ccb5dee35d01
suneeldeveloper <suneeldeveloper11@gmail.com>
2024-11-30 20:28:54 +0500

    update store

--- app/components/Store/ProductSlider.tsx
+++ app/components/Store/ProductSlider.tsx
@@ -1,9 +1,5 @@
 "use client";
 import React, { useState } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/swiper-bundle.css";
-import { Pagination } from "swiper/modules";
 import ShareModal from "../Modals/ShareModal";
 import ChartModal from "../Nutrition/ChartModal";
 import DownloadModal from "../Modals/DownloadModal";
@@ -64,7 +60,6 @@ const ProductsSlider: React.FC = () => {
   const [isOpenAddRatingModal, setIsOpenAddRatingModal] =
     useState<boolean>(false);
   const [showReviewsIndex, setShowReviewsIndex] = useState<number | null>(null);
-  const [lastSlideIndex, setLastSlideIndex] = useState<number | null>(null);
 
   // Handle showing reviews for a specific slide
   const handleToggleReviews = (index: number) => {
@@ -73,21 +68,6 @@ const ProductsSlider: React.FC = () => {
     } else {
       setShowReviewsIndex(index);
     }
-  };
-
-  // Handle slide change (prev/next slides only)
-  const handleSlideChange = (swiper: any) => {
-    const currentIndex = swiper.activeIndex;
-
-    if (
-      lastSlideIndex !== null &&
-      (currentIndex === lastSlideIndex - 1 ||
-        currentIndex === lastSlideIndex + 1)
-    ) {
-      setShowReviewsIndex(null);
-    }
-
-    setLastSlideIndex(currentIndex);
   };
 
   // Open Add Rating Window
@@ -143,136 +123,114 @@ const ProductsSlider: React.FC = () => {
 
   return (
     <div className="mx-auto relative z-10 px-5 md:px-0">
-      <Swiper
-        spaceBetween={20}
-        centeredSlides={true}
-        pagination={{ clickable: true }}
-        autoplay={{
-          delay: 3500,
-          disableOnInteraction: false,
-        }}
-        loop={true}
-        breakpoints={{
-          10: {
-            slidesPerView: 1,
-          },
-          768: {
-            slidesPerView: 2,
-          },
-          1024: {
-            slidesPerView: 3,
-          },
-        }}
-        modules={[Pagination]}
-        className="mySwiper"
-        onSlideChange={handleSlideChange}
-      >
+      <div className="max-w-[1000px] w-full mx-auto flex flex-col gap-5">
         {nutritionproducts.map((nutrition, index) => (
-          <SwiperSlide key={index}>
-            <div className="bg-white logomenubg rounded-2xl md:rounded-[30px] p-5 mb-12 md:mb-16">
-              {/* Product Image  */}
-              <div className="rounded-2xl overflow-hidden h-[320px] md:h-[384px] mb-4 bg-[#0000000F] p-3 relative">
-                <img
-                  src={nutrition.image}
-                  alt={nutrition.name}
-                  className="h-full bg-contain mx-auto relative"
-                />
+          <div
+            key={index}
+            className="bg-white logomenubg rounded-2xl md:rounded-[30px] p-5 mb-12 md:mb-16"
+          >
+            {/* Product Image  */}
+            <div className="rounded-2xl overflow-hidden h-[320px] md:h-[384px] mb-4 bg-[#0000000F] p-3 relative">
+              <img
+                src={nutrition.image}
+                alt={nutrition.name}
+                className="h-full bg-contain mx-auto relative"
+              />
 
-                <div className="absolute left-0 right-0 mx-auto bottom-5 flex gap-2 md:gap-3 justify-center w-full z-40">
-                  {/* Share btn  */}
-                  <button
-                    onClick={handleShareModal}
-                    className="logomenubg bg-white relative rounded-lg md:rounded-[14px] w-10 md:w-14 h-10 md:h-14 flex items-center justify-center"
-                  >
-                    <Image
-                      src="/assets/icon/arrow-share.svg"
-                      alt="arrow-share"
-                      loading="lazy"
-                      width={18}
-                      height={16}
-                    />
-                  </button>
-
-                  {/* Favourtes btn  */}
-                  <button
-                    onClick={handleOpenFavoritesModal}
-                    className="logomenubg bg-white rounded-lg md:rounded-[14px] w-10 md:w-14 h-10 md:h-14 flex items-center justify-center"
-                  >
-                    <Image
-                      src="/assets/icon/favioures-iocn.svg"
-                      alt="heart"
-                      loading="lazy"
-                      width={18}
-                      height={16}
-                    />
-                  </button>
-
-                  {/* Reviews btn  */}
-                  <button
-                    onClick={() => handleToggleReviews(index)}
-                    className="logomenubg bg-white rounded-lg md:rounded-[14px] px-3 md:px-7 h-10 md:h-14 flex items-center justify-center gap-[10px] font-medium text-base md:text-xl"
-                  >
-                    <Image
-                      src="/assets/icon/star-tag-2.svg"
-                      alt="star-tag"
-                      loading="lazy"
-                      width={18}
-                      height={16}
-                    />
-                    Reviews (40)
-                  </button>
-                </div>
-              </div>
-
-              {/* Product Details  */}
-
-              <div>
-                <h2 className="text-lg font-semibold">{nutrition.name}</h2>
-
-                <ul className="flex flex-col gap-2 my-3">
-                  <li className="flex items-center justify-between text-sm">
-                    <p>Brand</p>
-                    <p className="font-semibold">{nutrition.brand}</p>
-                  </li>
-
-                  <li className="flex items-center justify-between text-sm">
-                    <p className="text-sm">Price</p>
-                    <p className="font-semibold text-[#FC2424] line-through">
-                      {nutrition.price}
-                    </p>
-                  </li>
-
-                  <li className="flex items-center justify-between text-sm">
-                    <p className="text-sm">Discounted Price</p>
-                    <p className="font-semibold text-[#16AE8A] underline">
-                      {nutrition.discountprice}
-                    </p>
-                  </li>
-                </ul>
-
-                <Link
-                  href={nutrition.link}
-                  target="_blank"
-                  className="w-full rounded-[14px] h-14 bg-gradient flex items-center gap-2 justify-center p-4 text-white font-medium"
+              <div className="absolute left-0 right-0 mx-auto bottom-5 flex gap-2 md:gap-3 justify-center w-full z-40">
+                {/* Share btn  */}
+                <button
+                  onClick={handleShareModal}
+                  className="logomenubg bg-white relative rounded-lg md:rounded-[14px] w-10 md:w-14 h-10 md:h-14 flex items-center justify-center"
                 >
-                  Buy Now
-                </Link>
-              </div>
+                  <Image
+                    src="/assets/icon/arrow-share.svg"
+                    alt="arrow-share"
+                    loading="lazy"
+                    width={18}
+                    height={16}
+                  />
+                </button>
 
-              {/* Reviews  */}
-              <div className="bg-[#CEBAFD52] rounded-[14px]">
-                {/* Reviews  */}
-                {showReviewsIndex === index && (
-                  <div className="p-3 md:p-5 mt-5">
-                    <ReviewsForm onOpen={handleOpenAddRatingModal} />
-                    <ReviewsList />
-                  </div>
-                )}
+                {/* Favourtes btn  */}
+                <button
+                  onClick={handleOpenFavoritesModal}
+                  className="logomenubg bg-white rounded-lg md:rounded-[14px] w-10 md:w-14 h-10 md:h-14 flex items-center justify-center"
+                >
+                  <Image
+                    src="/assets/icon/favioures-iocn.svg"
+                    alt="heart"
+                    loading="lazy"
+                    width={18}
+                    height={16}
+                  />
+                </button>
+
+                {/* Reviews btn  */}
+                <button
+                  onClick={() => handleToggleReviews(index)}
+                  className="logomenubg bg-white rounded-lg md:rounded-[14px] px-3 md:px-7 h-10 md:h-14 flex items-center justify-center gap-[10px] font-medium text-base md:text-xl"
+                >
+                  <Image
+                    src="/assets/icon/star-tag-2.svg"
+                    alt="star-tag"
+                    loading="lazy"
+                    width={18}
+                    height={16}
+                  />
+                  Reviews (40)
+                </button>
               </div>
             </div>
-          </SwiperSlide>
+
+            {/* Product Details  */}
+
+            <div>
+              <h2 className="text-lg font-semibold">{nutrition.name}</h2>
+
+              <ul className="flex flex-col gap-2 my-3">
+                <li className="flex items-center justify-between text-sm">
+                  <p>Brand</p>
+                  <p className="font-semibold">{nutrition.brand}</p>
+                </li>
+
+                <li className="flex items-center justify-between text-sm">
+                  <p className="text-sm">Price</p>
+                  <p className="font-semibold text-[#FC2424] line-through">
+                    {nutrition.price}
+                  </p>
+                </li>
+
+                <li className="flex items-center justify-between text-sm">
+                  <p className="text-sm">Discounted Price</p>
+                  <p className="font-semibold text-[#16AE8A] underline">
+                    {nutrition.discountprice}
+                  </p>
+                </li>
+              </ul>
+
+              <Link
+                href={nutrition.link}
+                target="_blank"
+                className="w-full rounded-[14px] h-14 bg-gradient flex items-center gap-2 justify-center p-4 text-white font-medium"
+              >
+                Buy Now
+              </Link>
+            </div>
+
+            {/* Reviews  */}
+            <div className="bg-[#CEBAFD52] rounded-[14px]">
+              {/* Reviews  */}
+              {showReviewsIndex === index && (
+                <div className="p-3 md:p-5 mt-5">
+                  <ReviewsForm onOpen={handleOpenAddRatingModal} />
+                  <ReviewsList />
+                </div>
+              )}
+            </div>
+          </div>
         ))}
-      </Swiper>
+      </div>
 
       {/* Share Modal */}
       {isShareModalOpen && <ShareModal onClose={handleCloseShareModal} />}
