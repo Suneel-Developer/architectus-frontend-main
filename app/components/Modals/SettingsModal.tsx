2951802a736b57d72e37f9549326f68b3dd0ff1b
suneeldeveloper <suneeldeveloper11@gmail.com>
2024-11-30 20:09:24 +0500

    update setting window

--- app/components/Modals/SettingsModal.tsx
+++ app/components/Modals/SettingsModal.tsx
@@ -35,7 +35,7 @@ const SettingsModal: React.FC<ModalProps> = ({ onClose }) => {
     <div className="fixed inset-0 flex items-center justify-center z-50 p-5 windows-bg">
       <div onClick={onClose} className="absolute inset-0"></div>
 
-      <div className="h-full overflow-y-scroll overflow-x-hidden formscrollbar rounded-[30px] overflow-hidden flex justify-self-center">
+      <div className="h-full w-full md:w-auto overflow-y-scroll overflow-x-hidden formscrollbar rounded-[30px] overflow-hidden flex justify-self-center">
         <div className="bg-white rounded-[30px] px-5 py-[30px] w-full m-auto md:min-w-[690px] max-w-[690px] relative">
           <h1 className="font-semibold text-xl text-center mb-3">Settings</h1>
 
